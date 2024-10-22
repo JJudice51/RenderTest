62945cd65d3876574a96621b7feb27c101a95738
Justin Judice <142815014+JJudice51@users.noreply.github.com>
2024-10-22 14:06:13 -0500

    Added logs and Vertex Array

--- RenderStuff.cpp
+++ RenderStuff.cpp
@@ -7,13 +7,64 @@
 #include <sstream>
 #include "string_view"
 
+#define ASSERT(x) if (!(x)) __debugbreak()
+#define GlCall(x) GLCleatError();\
+    x;\
+    
+
+
+void GLDebugCallback(
+    unsigned int source,
+    unsigned int type,
+    unsigned int id,
+    unsigned int severity,
+    int length,
+    const char* message,
+    const void* userParam)
+{
+    const char* severityString = " ";
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        severityString = "[NOTIFICATION]";
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        severityString = "[LOW]";
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        severityString = "[MEDIUM]";
+        break;
+    case GL_DEBUG_SEVERITY_HIGH:
+        severityString = "[HIGH]";
+        break;
+    default:
+        severityString = "[DEBUG]";
+    }
+    std::cout << "[OpenGL] " << message << "\n";
+    if (severity > GL_DEBUG_SEVERITY_MEDIUM)
+    __debugbreak();
+}
+
+static void GlClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+
+
+
+void GLFWErrorCallback(int error, const char* description)
+{
+    std::cerr << description << "/n";
+};
+
+
 
 struct ShaderProgramSource
 {
     std::string VertexSource;
     std::string FragmentSource;
 };
-
 
 /*ALL CAPS COMMENTS ARE FOR PERSONAL USE OF REMEMBERING WHAT DOES WHAT*/
 
@@ -101,6 +152,9 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
    return program;
 }
 
+
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -108,6 +162,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -120,11 +178,25 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /*Makes frame rate = to the fps of the monitor running the program*/
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
+    {
         std::cout << "Error! GLEW not OK" << std::endl;
+    };
+     
+    
     
     std::cout << glGetString(GL_VERSION) << std::endl;
     
+
+    //GLFW error callback
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glEnable(GL_DEBUG_OUTPUT);
+    glfwSetErrorCallback(GLFWErrorCallback);
+    glDebugMessageCallback(GLDebugCallback, nullptr);
+
     float positions[] = {
         -0.5f, -0.5f, // 0
          0.5f, -0.5f, // 1
@@ -136,6 +208,11 @@ int main(void)
         0, 1, 2,
         2, 3, 0
     };
+
+    ///vertex array object
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
     /*CREATING AND BINDING THE VERTEX BUFFER*/
     unsigned int buffer;
@@ -158,32 +235,57 @@ int main(void)
     std::cout << source.VertexSource << "\n";
     std::cout << "FRAGMENT" << "\n";
     std::cout << source.FragmentSource << "\n";
+    
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
+    
+    glBindVertexArray(0);
+    glUseProgram(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 
     
     /*DRAW CODE FOR SHADER STUFF*/
    // glDrawElements(GL_TRIANGLES, 3);
 
+    float r = 0.0f;
+    float increment = 0.5f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+
+        glBindVertexArray(vao);
+      //  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+       //glEnableVertexAttribArray(0);
+      //  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
         
         /*DRAW CODE WITHOUT SHADER STUFF*/
        // glDrawArrays(GL_TRIANGLES, 0, 6);
 
         /*DRAW WITH SHADER STUFF*/
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); /*pulls the 6 indices from ibo and since it is bound above we can just put nullptr for the indices ptr*/
+        
+        if (r > 1.0f)
+            increment = -0.5f;
+        else if (r < 0.0f)
+            increment = 0.5f;
 
-        /*LEGACY OPENGL DRAW CODE GLEW NOT NEEDED*/
-        // glBegin(GL_TRIANGLES);
-        // glVertex2f(-0.5f, -0.5f);
-        // glVertex2f( 0.5f, 0.5f);
-        // glVertex2f( 0.5f, -0.5f);
-        // glEnd();
+        r += increment;
+
+
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
